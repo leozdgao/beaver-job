f0d514fa0c42425b6bd41114ba3e7d50a751fe74
leozdgao <gzdleo0816@outlook.com>
2023-10-13 00:57:27 +0800

    feat: tracing and add log

--- beaver-start/src/main/java/me/leozdgao/beaver/Application.java
+++ beaver-start/src/main/java/me/leozdgao/beaver/Application.java
@@ -1,55 +1,23 @@
 package me.leozdgao.beaver;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import me.leozdgao.beaver.config.DispatcherModule;
-import me.leozdgao.beaver.spi.BeaverProperties;
-import me.leozdgao.beaver.infrastructure.config.JsonModule;
-import me.leozdgao.beaver.infrastructure.config.PersistenceModule;
-import me.leozdgao.beaver.spi.TaskPersistenceQueryService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zhendong.gzd
  */
+@RestController
 @SpringBootApplication(scanBasePackages = {"me.leozdgao.beaver.adaptor.*"})
 public class Application {
+    @GetMapping("/health")
+    public String health() {
+        return "success";
+    }
+
     public static void main(String[] args) throws Exception {
         // 正常而言这里应该是启动 Web 应用
         SpringApplication.run(Application.class, args);
-
-        // demo();
-    }
-
-    private static void demo() throws IOException {
-        BeaverProperties properties = BeaverProperties.loadFromFile();
-        Injector injector = Guice.createInjector(
-            new DispatcherModule(),
-            new PersistenceModule(properties),
-            new JsonModule()
-        );
-
-        TaskPersistenceQueryService taskPersistenceQueryService =
-                injector.getInstance(TaskPersistenceQueryService.class);
-
-        // TaskConverter taskConverter = injector.getInstance(TaskConverter.class);
-
-        // TaskDispatcher taskDispatcher = injector.getInstance(TaskDispatcher.class);
-        // taskDispatcher.init();
-        //
-        // TaskMapper taskMapper = injector.getInstance(TaskMapper.class);
-        // List<TaskDO> lists = taskMapper.findTaskPage(null);
-        // System.out.println(lists);
-
-        //for (int i = 0; i < 10; i++) {
-        //    Task task = new Task();
-        //    taskDispatcher.accept(task);
-        //}
-        //
-        //Thread.sleep(5000);
     }
 }
